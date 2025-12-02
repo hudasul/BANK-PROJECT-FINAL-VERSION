d491cc940e76e43470ba35f38c952004077724a2
Your MujtabaSultan <b.mojtabaalilol@gmail.com>
2025-12-02 16:50:31 +0300

    this is cheating but its an interface , i dont feel like doing the whle thing all over again

---
+++ src/com/project/bank/IAccount.java
@@ -0,0 +1,9 @@
+package com.project.bank;
+
+import java.util.ArrayList;
+
+public interface IAccount {
+
+
+    public void addTransaction(Transaction t);
+}
--- src/com/project/bank/SavingAccount.java
+++ src/com/project/bank/SavingAccount.java
@@ -1,6 +1,6 @@
 package com.project.bank;
 
-public class SavingAccount extends Account{
+public class SavingAccount extends Account implements IAccount{
     public SavingAccount(String accountId, String customerId, DebitCard debitCard) {
         super(accountId, customerId, "Saving", true, 0, debitCard);
     }
